4a2f4036f220467ffbc9d95084339971a57826bd
Andreas Gruhler <agruhl@gmx.ch>
2017-09-16 20:57:54 +0200

    change home views to warning views

--- app/src/main/java/hack/abtoerner/abtoerner/Home.java
+++ app/src/main/java/hack/abtoerner/abtoerner/Home.java
@@ -40,7 +40,7 @@ public class Home extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_warning);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
