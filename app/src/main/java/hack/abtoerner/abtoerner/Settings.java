215a98ea311bcc03cb0bcf57928fa1831a96a79d
Christoph <christoph.schildknecht@bluemail.ch>
2017-09-17 03:02:26 +0200

    mach ned kabut

--- app/src/main/java/hack/abtoerner/abtoerner/Settings.java
+++ app/src/main/java/hack/abtoerner/abtoerner/Settings.java
@@ -2,6 +2,8 @@ package hack.abtoerner.abtoerner;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 
 public class Settings extends AppCompatActivity {
 
@@ -10,4 +12,15 @@ public class Settings extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
     }
+
+/*    Switch simpleSwitch = (Switch) findViewById(R.id.switch1);
+        simpleSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+        public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+            if (isChecked) {
+                System.out.println("Toggle switch enabled!");
+            } else {
+                System.out.println("Toggle switch disabled!");
+            }
+        }
+    });*/
 }
